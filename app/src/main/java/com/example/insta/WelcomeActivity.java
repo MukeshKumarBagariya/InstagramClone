f1f4690a542ccbde8838f5a4c17616d39cd037b4
krmickey <krmickey1@gmail.com>
2019-12-08 20:56:13 +0530

    Adding UserTab,ProfileTab and SharePictureTab

--- app/src/main/java/com/example/insta/WelcomeActivity.java
+++ app/src/main/java/com/example/insta/WelcomeActivity.java
@@ -1,16 +1,34 @@
 package com.example.insta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
 public class WelcomeActivity extends AppCompatActivity {
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private TabAdapter tabAdapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         setTitle("Home Page");
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewpager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager,false);
+
 
     }
 }
